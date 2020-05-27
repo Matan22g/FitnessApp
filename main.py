8a867d91ad7b81794019ae399e2e96e9a9feba67
Matan Achiel <MATANACHIEL@MAIL.TAU.AC.IL>
2020-05-27 19:59:08 +0300

    removed unnecessary code.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from FirebaseLoginScreen.firebaseloginscreen import FirebaseLoginScreen
 import kivy.utils as utils
 import requests
 import json
+from kivymd.uix.picker import MDTimePicker, MDThemePicker
 
 Window.size = (420, 630)
 
@@ -19,6 +20,9 @@ class HomeScreen(Screen):
 
     def on_enter(self, *args):
         self.app.title = "Home"
+
+    def addworkout(self, *args):
+        self.app.change_screen1("workoutsscreen")
 
     # def is_binary(self, binary_number):
     #     try:
@@ -64,28 +68,29 @@ class SettingsScreen(Screen):
         else:
             self.app.theme_cls.theme_style = "Light"
 
+    def show_theme_picker(self):
+        theme_dialog = MDThemePicker()
+        theme_dialog.open()
+
 
 class WorkoutsScreen(Screen):
     def __init__(self, **kw):
         super().__init__(**kw)
         self.app = MDApp.get_running_app()
-        self.limit = 5
-        self.Exercise = 0
+        self.limit = 12
+        self.exercise = 0
 
     def on_enter(self, *args):
         self.app.title = "Workouts"
 
-    def addworkout(self):
-        limit = self.limit
-        if self.Exercise < limit:
-            self.Exercise += 1
-            exid = "ex" + str(self.Exercise)
-            self.ids[exid].opacity = 1
+    def addexercise(self):
+        # Add Another exercise
 
-            # pos = {"x": .35, "y": heig}
-            # b1 = MDTextField(id=str(self.Exercise), text="name of workout", pos_hint=pos, size_hint= (.3, .1))
-            # print (b1)
-            # self.add_widget(b1)
+        limit = self.limit
+        if self.exercise < limit:
+            self.exercise += 1
+            exid = "ex" + str(self.exercise)
+            self.ids[exid].opacity = 1
         else:
             msg = "Cannot add more than\n" + str(limit) + " exercises at the moment."
             self.app.dialog = MDDialog(text=msg, radius=[10, 7, 10, 7], size_hint=(0.5, None))
@@ -93,48 +98,56 @@ class WorkoutsScreen(Screen):
 
     def deleteworkout(self):
         limit = self.limit
-        if self.Exercise > 0:
-            exid = "ex" + str(self.Exercise)
-            self.Exercise -= 1
+        if self.exercise > 0:
+            exid = "ex" + str(self.exercise)
+            self.exercise -= 1
             self.ids[exid].opacity = 0
 
     def workoutvalid(self):
         msg = ""
         if len(self.ids["ex0"].text) == 0:
-            msg = "Choose a name for the workout"
-        elif self.Exercise == 0:
+            msg = "Choose name for the workout"
+        elif self.exercise == 0:
             msg = "Enter at least one exercise"
         else:
-            for i in range(self.Exercise):
+            for i in range(self.exercise):
                 exid = "ex" + str(i + 1)
                 if len(self.ids[exid].text) == 0:
                     msg = "Name all the exercises"
                     break
         return msg
 
+    def clearworkout(self):
+        pass
+
     def saveworkout(self):
         msg = self.workoutvalid()
         if len(msg) == 0:
-            self.app.change_screen1("homescreen")
             exercises = []
             workout_name = self.ids["ex0"].text
-            for i in range(self.Exercise):
+            for i in range(self.exercise):
                 exid = "ex" + str(i + 1)
                 exercises.append(self.ids[exid].text)
-            for i in range(self.Exercise + 1):
+            for i in range(self.exercise + 1):
                 exid = "ex" + str(i)
                 self.ids[exid].text = ""
                 self.ids[exid].focus = False
                 if i > 0:
                     self.ids[exid].opacity = 0
-            self.Exercise = 0
-            print(exercises)
+            self.exercise = 0
+            Workout = "{%s: %s}" % (workout_name, exercises)
+            self.uploadWorkout(Workout)
+            print(Workout)
             self.app.change_screen1("homescreen")
             return True
         else:
             self.app.dialog = MDDialog(text=msg, radius=[10, 7, 10, 7], size_hint=(0.5, None))
             self.app.dialog.open()
             return False
+
+    def uploadWorkout(self, workout):
+        workout_request = requests.post("https://gymbuddy2.firebaseio.com/%s/workouts.json?auth=%s"
+                                        % (self.app.local_id, self.app.id_token), data=json.dumps(workout))
 
 
 class FriendsScreen(Screen):
@@ -150,6 +163,13 @@ class FriendsScreen(Screen):
 class MainApp(MDApp):
     my_friend_id = ""
     friends_list = ""
+    id_token = ""
+    local_id = ""
+
+    def __init__(self, **kwargs):
+        self.title = "KivyMD Example - Backdrop"
+        self.theme_cls.primary_palette = "Indigo"
+        super().__init__(**kwargs)
 
     def set_friend_id(self, my_friend_id):
         self.my_friend_id = my_friend_id
@@ -171,7 +191,18 @@ class MainApp(MDApp):
         screen_manager = self.root.ids
 
     def on_start(self):
-        pass
+        self.root.ids['nav_drawer'].swipe_edge_width = -2
+        # try:
+        #     # Try to read the persisten signin credentials (refresh token)
+        #     with open("refresh_token.txt", 'r') as f:
+        #         refresh_token = f.read()
+        #     # Use refresh token to get a new idToken
+        #     id_token, local_id = self.my_firebase.exchange_refresh_token(refresh_token)
+        #     self.local_id = local_id
+        #     self.id_token = id_token
+        # except Exception as e:
+        #     print(e)
+        #     pass
         # Get database data
         # result = requests.get("https://gymbuddy2.firebaseio.com/" + str(self.my_friend_id) + ".json")
         # print(json.loads(result.content.decode()))
